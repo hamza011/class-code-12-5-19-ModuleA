36da96445294cddc34c16fd7c5dbd60327affa3d
Hamza Ahmed <h.hamza011@gmail.com>
2019-05-16 10:25:38 +0500

    'first-commit'

---
+++ index.js
@@ -0,0 +1,91 @@
+var student1 = {
+    name: "Taha",
+    age: 15,
+    study: function () {
+        console.log("study");
+    },
+    bool: true,
+    arr: ['abc', 'xyz', 23],
+    subjects: {
+        maths: "Maths"
+    }
+
+};
+// console.log(student1.subjects.maths);
+// console.log(student1.arr[1]);
+
+
+// var student2 = {
+//     name: "Ali",
+//     age: 20
+// };
+// var student3 = {
+//     name: "Hamza",
+//     // age: 20
+// };
+// student2['na me'] = "XYZ";
+// console.log(student2['na me']);
+// console.log(student3);
+// student3.name = "Student3"
+// console.log(student3);
+// // console.log(student2['age']);
+// // console.log(student1.study());
+
+// // var arr = [12, 32, 43]
+// // arr[0];
+// // arr.0
+
+// var 0name = "Taha";
+// window['0na me'] = "ABC";
+// document['getElementById']('id')
+// // var age = 15;
+
+// var students = [];
+// function register() {
+//     students.push(
+//         {
+//             name: 'studentName',
+//             age: 'studentAge'
+//         }
+//     )
+// }
+
+// var stu = {
+//     name: "Hamza",
+//     age: 20
+// };
+// console.log('age' in stu);
+// for(var key in stu){
+//     // console.log("KEY ", key)
+//     // stu[key] = 'xyz';
+//     console.log(stu[key]); // stu.name, stu.age
+// }
+
+// var stu = [
+//     {
+//         name: "Hamza",
+//         age: 20
+//     },
+//     {
+//         name: "Taha",
+//         age: 20
+//     },
+// ];
+// for (var value of stu) {
+//     console.log(value);
+// }
+
+// delete stu.age;
+// delete stu.name;
+// console.log(stu);
+
+// var todos = [
+//     {
+//         todoValue: "Todo val 1",
+//         complete: true
+//     },
+//     {
+//         todoValue: "Todo val 2",
+//         complete: false
+//     },
+// ];
